ada74fd6a5a1da3a70cdbf37742d97aba70902bc
JuanRendon56 <A01731524@itesm.mx>
2023-05-16 14:20:22 -0600

    Commit Apertura

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import ChatGpt from './chatgpt.png';
+import Google from './google.png';
+import PowerPoint from './Pp.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="Proyect-box">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Sigue lo siguientes pasos:
+        <br/>
+        1) Abre ChatGPT (crea una cuenta de ser necesario) e introduce el tema que desees que genere. 
+        <br/>
+        2) Se recomienda utilizar oraciones como “Genera un esquema Powerpoint de X diapositivas sobre [TEMA]”
+        <br/> 
+        3) Abre GoogleImages e introduce la palabra clave con respecto a tu presentación y descarga las imagenes que creas útiles
+        <br/>
+        4) Edita e implementa tu Powerpoint
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <a href="https://chat.openai.com/" target="/blank">
+        <img src={ChatGpt} alt="ChatGPT" width="200" height="200"></img>
+      </a>
+      <a href="https://images.google.com/" target="/blank">
+        <img src={Google} alt="GoogleImgs" width="400" height="200"></img>
+      </a>
+      <img src={PowerPoint} alt="PowerPoint" width="200" height="200"></img>
     </div>
   );
 }

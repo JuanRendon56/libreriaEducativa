b3661a39def5b7b454e828dded3b950b16dc94b9
JuanRendon56 <A01731524@itesm.mx>
2023-06-02 17:06:31 -0600

    Base de datos conectada con el front!

--- back/index.js
+++ back/index.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const app = express()
-const mysql = require('mysql')
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const db = mysql.createPool({
   host: 'localhost',
@@ -9,10 +11,20 @@ const db = mysql.createPool({
   database: 'libreriacrud'
 });
 
-app.get('/', (req, res) => {
-  const sqlInsert = "INSERT INTO entradas (titulo, autor, guia, docs, date) VALUES ('TEST2', 'Juan', 'MIDMAIDAMDIASMIMDAIDAIMD', 0, 4);";
-  db.query(sqlInsert, (err, result) => {
-    res.send("Mande datos!");
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json());
+app.use(cors());
+
+app.post('/agregar', (req, res) => {
+  const titulo = req.body.titulo;
+  const autor = req.body.autor;
+  const guia = req.body.guia;
+  const docs = req.body.docs;
+  const date = req.body.date;
+
+  const sqlInsert = "INSERT INTO entradas (titulo, autor, guia, docs, date) VALUES (?,?,?,?,?);";
+  db.query(sqlInsert,  [titulo, autor, guia, docs, date], (err, result) => {
+    res.send(result);
   })
 });
 
--- front/src/components/AddElement.js
+++ front/src/components/AddElement.js
@@ -2,6 +2,7 @@ import React, { useContext} from 'react';
 import ElementForm from './ElementForm';
 import { useNavigate } from 'react-router-dom';
 import ElementsContext from '../context/ElementsContext';
+import Axios from "axios";
 
 const AddElement = () => {
   let navigate = useNavigate();
@@ -9,6 +10,15 @@ const AddElement = () => {
   const handleOnSubmit = (entrada) => {
     //Se añade entrada al inicio de la lista (Se puede editar para poner hasta abajo)
     setEntradas([entrada, ...entradas]);
+    Axios.post("http://localhost:3001/agregar", {
+      titulo: entrada.titulo, 
+      autor: entrada.autor, 
+      guia: entrada.guia, 
+      docs: 1, 
+      date: 3
+    }).then(() => {
+      alert("Se pudo!");
+    });
     navigate('/');
   };
 

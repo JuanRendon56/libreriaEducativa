dfbcfd4dd8f357e5ba1f1b298d474cee319f7bc7
JuanRendon56 <A01731524@itesm.mx>
2023-06-03 17:46:47 -0600

    GET implementado en List

--- front/src/components/List.js
+++ front/src/components/List.js
@@ -2,10 +2,16 @@ import ElementsContext from '../context/ElementsContext';
 import React, { useContext} from 'react';
 import _ from 'lodash';
 import Entrada from './Element';
+import Axios from 'axios';
 
 const List = () => {
-  //Elimina elementos cuyo id no sea igual al registro
   const { entradas, setEntradas} = useContext(ElementsContext);
+  //Obtiene entradas de la base de datos
+  Axios.get("http://localhost:3001/obtener").then((response)=> {
+      setEntradas(response.data);
+  }); 
+
+  //Elimina elementos cuyo id no sea igual al registro
   const handleRemoveElement = (id) => {
     setEntradas(entradas.filter((entrada) => entrada.id !== id));
   };
